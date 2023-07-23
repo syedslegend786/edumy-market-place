3a3e72de80e4e57570d98e08575c7b8339646566
syedslegend786 <syedslegend786@gmail.com>
2023-07-23 18:04:08 +0500

    middlware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,62 +1,50 @@
-import { getToken } from "next-auth/jwt"
-import { withAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
-    async function middleware(req) {
-        const token = await getToken({ req })
-        const isAuth = !!token
-        const isAuthPage =
-            req.nextUrl.pathname.startsWith("/auth/login") ||
-            req.nextUrl.pathname.startsWith("/auth/register")
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith("/auth/login") ||
+      req.nextUrl.pathname.startsWith("/auth/register");
 
-        if (isAuthPage) {
-            if (isAuth) {
-                return NextResponse.redirect(new URL("/", req.url))
-            }
-            return null
-        }
-
-
-        // if (!isAuth) {
-        //     let from = req.nextUrl.pathname;
-        //     if (req.nextUrl.search) {
-        //         from += req.nextUrl.search;
-        //     }
-        //     return NextResponse.redirect(
-        //         new URL(`/auth/login?from=${encodeURIComponent(from)}`, req.url)
-        //     );
-        // }
-
-        const isInstructorPage =
-            req.nextUrl.pathname.startsWith("/instructor")
-
-        if (isInstructorPage) {
-            if (isAuth && !token.roles.includes("instructor")) {
-                let from = req.nextUrl.pathname;
-                if (req.nextUrl.search) {
-                    from += req.nextUrl.search;
-                }
-                return NextResponse.redirect(
-                    new URL(`/auth/login?from=${encodeURIComponent(from)}`, req.url)
-                );
-            }
-            return null
-        }
-    },
-    {
-        callbacks: {
-            async authorized() {
-                // This is a work-around for handling redirect on auth pages.
-                // We return true here so that the middleware function above
-                // is always called.
-
-                return true
-            },
-        },
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+      return null;
     }
-)
+
+    const isInstructorPage = req.nextUrl.pathname.startsWith("/instructor");
+
+    if (isInstructorPage) {
+      if (isAuth && !token.roles.includes("instructor")) {
+        let from = req.nextUrl.pathname;
+        if (req.nextUrl.search) {
+          from += req.nextUrl.search;
+        }
+        return NextResponse.redirect(
+          new URL(`/auth/login?from=${encodeURIComponent(from)}`, req.url)
+        );
+      }
+      return null;
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+
+        return true;
+      },
+    },
+  }
+);
 
 export const config = {
-    matcher: ["/", "/auth/login", "/auth/register", "/instructor"],
-}
+  matcher: ["/", "/auth/login", "/auth/register", "/instructor"],
+};
